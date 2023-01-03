374c8f588e2afcae9692f01b7932bc75184c563e
DesertStormII <119744433+DesertStormII@users.noreply.github.com>
2023-01-03 17:05:42 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "all even numbers";//all even numbers
+	for (int i = 0; i <= 200; i = i + 2)
+		cout << i<< "\t";
+
+
+	return 0;
+}
